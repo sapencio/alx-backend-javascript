d2880a1ceb7f55bc531f6ae61bbb619b146304f0
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-12-19 21:53:03 +0300

    Reformat the code

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
         {
             files: ["*.js"],
             excludedFiles: "babel.config.js",
-          },
+        },
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
