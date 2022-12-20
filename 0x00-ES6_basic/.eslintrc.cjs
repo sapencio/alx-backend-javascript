ffa1b7259a88bbbd3d2850a236809f4cce9da260
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-12-20 14:21:40 +0300

    Trying eslint --fix

---
+++ 0x00-ES6_basic/.eslintrc.cjs
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+        {
+            files: ["*.js"],
+            excludedFiles: "babel.config.js",
+        },
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
