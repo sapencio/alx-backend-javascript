61671f2527f02e66d3328e3d8536bf947cf72714
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-02-20 19:37:22 +0300

    Executing basic javascript with Node js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Tibebe Solomon <https://github.com/sapencio>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
